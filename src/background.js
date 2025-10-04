e199bcf14c472c4cae2b91d0b5efea27959a7f6d
Patrick Lewis <lewispjjpb@gmail.com>
2025-10-04 17:12:49 -0400

    basic css inject

--- src/background.js
+++ src/background.js
@@ -1,5 +1,54 @@
+const extensions = 'https://developer.chrome.com/docs/extensions';
+const webstore = 'https://developer.chrome.com/docs/webstore';
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Extension installed/updated');
+
+    // Add error handling
+    if (chrome.action) {
+        chrome.action.setBadgeText({
+            text: "OFF",
+        });
+        console.log('Badge text set to OFF');
+    } else {
+        console.error('chrome.action is not available');
+    }
+});
+
+
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
         text: "OFF",
     });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+    let nextState = 'OFF'; // Initialize with default value
+    
+    if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+        // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+        const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+        // Next state will always be the opposite
+        nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+        // Set the action badge to the next state
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState,
+        });
+    }
+    
+    if (nextState === "ON") {
+        // Insert the CSS file when the user turns the extension on
+        await chrome.scripting.insertCSS({
+            files: ["src/focus-mode.css"],
+            target: { tabId: tab.id },
+        });
+    } else if (nextState === "OFF") {
+        // Remove the CSS file when the user turns the extension off
+        await chrome.scripting.removeCSS({
+            files: ["src/focus-mode.css"],
+            target: { tabId: tab.id },
+        });
+    }
 });
